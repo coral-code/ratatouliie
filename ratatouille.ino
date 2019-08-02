4a4fd1c713ab322faa2ab25d350d112b8354457b
coral-code <53235773+coral-code@users.noreply.github.com>
2019-08-02 10:00:19 -0400

    Add files via upload

---
+++ ratatouille.ino
@@ -0,0 +1,47 @@
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+
+void loop(){
+ digitalWrite(LED_BUILTIN, HIGH);   
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);    // s
+  delay(1000); 
+   
+   digitalWrite(LED_BUILTIN, HIGH);   
+  delay(1000);
+   digitalWrite(LED_BUILTIN, LOW);   
+  delay(1000);
+   
+   digitalWrite(LED_BUILTIN, HIGH);   
+  delay(1000);
+   digitalWrite(LED_BUILTIN, LOW);   
+  delay(1000);
+
+ digitalWrite(LED_BUILTIN, LOW);    // space
+  delay(300); 
+
+   digitalWrite(LED_BUILTIN, HIGH);  //o 
+  delay(300);
+   digitalWrite(LED_BUILTIN, LOW);   
+  delay(100);
+  
+   digitalWrite(LED_BUILTIN, HIGH);   
+  delay(300);
+   digitalWrite(LED_BUILTIN, LOW);   
+  delay(100);
+   
+   digitalWrite(LED_BUILTIN, HIGH);   
+  delay(300);
+   digitalWrite(LED_BUILTIN,LOW);   
+  delay(1000);
+
+    digitalWrite(LED_BUILTIN, LOW);    // space
+  delay(300); 
+
+
+  }
+  
